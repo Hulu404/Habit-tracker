7d4688b078c6b5a052a6c768c592048466d24820
Hulu <hulumulu680@gmail.com>
2025-09-14 20:31:18 +0300

    Пока тупук

---
+++ files/handlers.py
@@ -0,0 +1,13 @@
+
+def scale(message):   # Функция создаёт шкалу и добавляет в неё прогресс поинты
+    name = message
+
+    def make_scale():
+        name_ = list()
+        return name_
+
+    make_scale()
+
+    return
+
+
---
+++ files/main.py
@@ -0,0 +1,7 @@
+from handlers import *
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
